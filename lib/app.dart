279894ce26d5e179bef896f9baa646810ab3e1dc
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2023-05-14 11:57:07 +0100

    Add Sign Up Page

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_pap/pages/login_page.dart';
+import 'package:my_pap/pages/sign_up_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -8,7 +9,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: SignUpPage(),
     );
   }
 }
