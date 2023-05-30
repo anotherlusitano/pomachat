99a031aeeb1f4f5d90199178143fe3bc9975b343
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2023-05-30 18:20:21 +0100

    Activate GetFriendId provider on app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:my_pap/auth/auth.dart';
+import 'package:my_pap/providers/get_friend_id.dart';
+import 'package:provider/provider.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: AuthPage(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => GetFriendId(),
+        ),
+      ],
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: AuthPage(),
+      ),
     );
   }
 }
