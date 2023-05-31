42bedc24631333ea5e969435535a3be8b91231ff
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2023-05-31 10:37:34 +0100

    Add function to create new private conversation

--- lib/pages/friends_page.dart
+++ lib/pages/friends_page.dart
@@ -3,6 +3,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:my_pap/components/message_post.dart';
 import 'package:my_pap/pages/private_conversation_page.dart';
+import 'package:my_pap/providers/get_friend_id.dart';
+import 'package:provider/provider.dart';
 
 class FriendsPage extends StatefulWidget {
   const FriendsPage({super.key});
@@ -13,6 +15,29 @@ class FriendsPage extends StatefulWidget {
 
 class _FriendsPageState extends State<FriendsPage> {
   final currentUser = FirebaseAuth.instance.currentUser;
+
+  void createPrivateConversation(String friendId) {
+    final List<String> userIds = [currentUser!.uid, friendId];
+    final conversationsCollection = FirebaseFirestore.instance.collection('PrivateConversations');
+
+    // Send two separate queries to verify if exists that conversatin
+    // (it's two queries because firestore don't allow to use two whereIn)
+    conversationsCollection.where('user1', whereIn: userIds).get().then((querySnapshot1) {
+      conversationsCollection.where('user2', whereIn: userIds).get().then((querySnapshot2) {
+        if (querySnapshot1.docs.isNotEmpty && querySnapshot2.docs.isNotEmpty) {
+          // Save conversation Id
+          Provider.of<GetFriendId>(context, listen: false).updateFriendId(querySnapshot2.docs[0].id);
+          return;
+        }
+
+        // Add new document to collection
+        conversationsCollection
+            .add({'user1': currentUser!.uid, 'user2': friendId})
+            .then((value) => print('Conversation added'))
+            .catchError((error) => print('Failed to add conversation: $error'));
+      }).catchError((error) => print('Failed to get querySnapshot2: $error'));
+    }).catchError((error) => print('Failed to get querySnapshot1: $error'));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -54,10 +79,14 @@ class _FriendsPageState extends State<FriendsPage> {
                                     final discriminator = snapshot.data!['discriminator'];
 
                                     return GestureDetector(
-                                      onTap: () => Navigator.push(
-                                        context,
-                                        MaterialPageRoute(builder: (context) => const PrivateConversationPage()),
-                                      ),
+                                      onTap: () {
+                                        createPrivateConversation(friendId);
+                                        Provider.of<GetFriendId>(context, listen: false).updateFriendId(friendId);
+                                        Navigator.push(
+                                          context,
+                                          MaterialPageRoute(builder: (context) => const PrivateConversationPage()),
+                                        );
+                                      },
                                       child: MessagePost(
                                         message: bio,
                                         user: "$username#$discriminator",
