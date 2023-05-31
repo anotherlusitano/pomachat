639dd2b03c39890ac27e48d15b7687d95d300a28
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2023-05-31 10:27:45 +0100

    Add GetPrivateConversationId provider

---
+++ lib/providers/get_private_conversation_id.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class GetPrivateConversationId extends ChangeNotifier {
+  String conversationId;
+  GetPrivateConversationId({
+    this.conversationId = '',
+  });
+
+  void updateFriendId(String newFriendId) {
+    conversationId = newFriendId;
+    notifyListeners();
+  }
+}
