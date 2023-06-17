173fec70ca4e6401b3f6e2c115cc1e0a1dd670f5
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2023-06-17 14:46:19 +0100

    Update the MessagePost widget to allow selection

--- lib/components/message_post.dart
+++ lib/components/message_post.dart
@@ -32,59 +32,61 @@ class MessagePost extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(8),
-      ),
-      margin: const EdgeInsets.only(top: 25, left: 25, right: 25),
-      padding: const EdgeInsets.all(25),
-      child: Row(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.grey[400],
-              shape: BoxShape.circle,
+    return SelectionArea(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        margin: const EdgeInsets.only(top: 25, left: 25, right: 25),
+        padding: const EdgeInsets.all(25),
+        child: Row(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.grey[400],
+                shape: BoxShape.circle,
+              ),
+              padding: const EdgeInsets.all(10),
+              child: const Icon(
+                Icons.person,
+                color: Colors.white,
+              ),
             ),
-            padding: const EdgeInsets.all(10),
-            child: const Icon(
-              Icons.person,
-              color: Colors.white,
-            ),
-          ),
-          const SizedBox(width: 20),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      user,
-                      style: TextStyle(
-                        color: Colors.grey[500],
-                      ),
-                    ),
-                    Row(
-                      children: [
-                        Text(
-                          timestamp ?? '',
-                          style: TextStyle(
-                            color: Colors.grey[500],
-                          ),
+            const SizedBox(width: 20),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        user,
+                        style: TextStyle(
+                          color: Colors.grey[500],
                         ),
-                        isUserMessage(),
-                      ],
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 10),
-                Text(message),
-              ],
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            timestamp ?? '',
+                            style: TextStyle(
+                              color: Colors.grey[500],
+                            ),
+                          ),
+                          isUserMessage(),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 10),
+                  Text(message),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
