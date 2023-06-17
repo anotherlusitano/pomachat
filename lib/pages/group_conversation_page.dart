825735589b1bc28b639b1f4e91fc269789a93371
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2023-06-17 14:58:20 +0100

    Add feature to send images in group conversations

--- lib/pages/group_conversation_page.dart
+++ lib/pages/group_conversation_page.dart
@@ -1,7 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:intl/intl.dart';
+import 'package:my_pap/components/image_post.dart';
 import 'package:my_pap/components/message_post.dart';
 import 'package:my_pap/components/validated_text_field.dart';
 import 'package:my_pap/providers/get_group_id.dart';
@@ -22,6 +25,8 @@ class _GroupConversationPageState extends State<GroupConversationPage> {
   final textController = TextEditingController();
 
   String? groupId;
+
+  late Reference ref;
 
   void postMessage() {
     if (textController.text.trim().isNotEmpty) {
@@ -85,9 +90,32 @@ class _GroupConversationPageState extends State<GroupConversationPage> {
                       itemBuilder: (context, index) {
                         // get the message
                         final message = snapshot.data!.docs[index];
+
                         final date = DateTime.fromMillisecondsSinceEpoch(message['timeStamp'].seconds * 1000);
                         final formattedDate = DateFormat('dd/MM/yyyy, HH:mm').format(date);
 
+                        if (message['message']
+                            .contains("https://firebasestorage.googleapis.com/v0/b/pap-gpsi.appspot.com/o/")) {
+                          return ImagePost(
+                            imageUrl: message['message'],
+                            user: message['user'],
+                            timestamp: formattedDate,
+                            currentUser: getUsername(),
+                            deleteMessage: () {
+                              FirebaseFirestore.instance
+                                  .collection('Groups')
+                                  .doc(groupId)
+                                  .collection('Messages')
+                                  .doc(message.id)
+                                  .delete()
+                                  .then(
+                                    (value) => print('Image deletada com sucesso'),
+                                    onError: (e) => print("Erro ao deletar imagem $e"),
+                                  )
+                                  .then((value) => ref.delete());
+                            },
+                          );
+                        }
                         return MessagePost(
                           message: message['message'],
                           user: message['user'],
@@ -133,6 +161,43 @@ class _GroupConversationPageState extends State<GroupConversationPage> {
                       obscureText: false,
                       maxLenght: 4000,
                     ),
+                  ),
+                  IconButton(
+                    onPressed: () async {
+                      //Create new instance
+                      final ImagePicker picker = ImagePicker();
+
+                      //Call this method to get image from users device
+                      final image = await picker.pickImage(source: ImageSource.gallery);
+                      if (image != null) {
+                        //Store it, we are going to use it later
+                        final fileBytes = await image.readAsBytes();
+
+                        // Create a reference to a file from a Google Cloud Storage URI
+                        Reference reference = FirebaseStorage.instance
+                            .ref()
+                            .child("${DateTime.now().microsecondsSinceEpoch}${image.name}");
+
+                        // save the reference to use them to delete from the storage
+                        setState(() {
+                          ref = reference;
+                        });
+
+                        // Waits till the file is uploaded then stores the download url
+                        final snapshot = await reference.putData(fileBytes);
+                        String downloadUrl = await snapshot.ref.getDownloadURL();
+
+                        //Upload to Firebase
+                        FirebaseFirestore.instance.collection('Groups').doc(groupId).collection('Messages').add({
+                          'user': getUsername(),
+                          'message': downloadUrl,
+                          'timeStamp': Timestamp.now(),
+                        });
+                      } else {
+                        print('No Image Path Received');
+                      }
+                    },
+                    icon: const Icon(Icons.photo),
                   ),
                   IconButton(
                     onPressed: postMessage,
